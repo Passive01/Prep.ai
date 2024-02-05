fb7c9c4072255dcddac593f30ccd212d837619ca
root <root@Shahzaib>
2024-02-05 22:45:34 +0500

    added youtube transcript

---
+++ backend/app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from langchain_community.document_loaders import YoutubeLoader
+from youtube_transcript_api import YouTubeTranscriptApi
+
+app = FastAPI() 
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all HTTP headers
+)
+
+@app.post("/")
+def get_item(youtube_link: dict):
+    loader = YoutubeLoader.from_youtube_url(
+    youtube_link["youtubeLink"], add_video_info=False
+)
+    loader.load()
+    #transcript_string = loader.page_content
+    print(loader.load())
+    return loader.load()[0].page_content
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios';
 import './App.css'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [youtubeContent,setYoutubeContent] = useState("")
+  const [youtubeLink, setYoutubeLink] = useState("")
+
+  const handlePostRequest = async () => {
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/', {
+        // Your POST data goes here
+        youtubeLink
+      });
+
+      setYoutubeContent(response.data);
+    } catch (error) {
+      console.error('Error making POST request:', error);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Enter YouTube URL</h2>
+      <TextField id="outlined-basic" label="Youtube URL" variant="outlined" value={youtubeLink} onChange={(e)=>setYoutubeLink(e.target.value)}/>
+      <Button variant="outlined" style={{margin: "12px"}} onClick={handlePostRequest}>Send</Button>
+      {youtubeContent && (
+        <div>
+          <h2>Response Data:</h2>
+          <b>{youtubeContent}</b>
+        </div>
+      )}
     </>
   )
 }
