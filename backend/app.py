d009b9bc5197030cb1344290b6ab190fda78e03e
root <root@Shahzaib>
2024-02-08 23:25:22 +0500

    added the feature of summarize the YT video

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,27 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from langchain_community.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain.retrievers import ParentDocumentRetriever
+from langchain.storage import InMemoryStore
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
+
+
+
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+embeddings_model = OpenAIEmbeddings(openai_api_key=os.getenv('OPENAI_API_KEY'))
 
 app = FastAPI() 
 
@@ -14,10 +35,53 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all HTTP headers
 )
 
+transcript = ""
+
 @app.post("/")
 def get_item(youtube_link: dict):
-    loader = YoutubeLoader.from_youtube_url(
-    youtube_link["youtubeLink"], add_video_info=False
-)
-    loader.load()
-    return loader.load()[0].page_content
+    try: 
+        loader = YoutubeLoader.from_youtube_url(
+        youtube_link["youtubeLink"], add_video_info=False
+        )
+        transcript = loader.load()[0].page_content
+        return transcript
+    except Exception as e:
+        print(f"Error: {e}")
+
+@app.post("/summarize")
+def summarize(text: dict):
+    try: 
+        #print(text["youtubeContent"])
+        text_splitter = RecursiveCharacterTextSplitter(
+            # Set a really small chunk size, just to show.
+            chunk_size=100,
+            chunk_overlap=20,
+            length_function=len,
+            is_separator_regex=False,
+        )
+        texts = text_splitter.split_text(text["youtubeContent"])
+
+        template = """Provide the summary based only on the following YouTube video. Include all the important parts of the video and ignore the extra content. Ignore the video outro if it does not provide any information about the topic:
+
+        {context}
+
+        Summary:
+        """
+        prompt = ChatPromptTemplate.from_template(template)
+        model = ChatOpenAI()
+
+        chain = (
+            {"context": RunnablePassthrough()}
+            | prompt
+            | model
+            | StrOutputParser()
+        )
+
+        response = chain.invoke(texts)
+        return response
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+    
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,21 @@ import axios from 'axios';
 import './App.css'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 
 
 function App() {
   const [youtubeContent,setYoutubeContent] = useState("")
+  const [youtubeSummary,setYoutubeSummary] = useState("")
   const [youtubeLink, setYoutubeLink] = useState("")
+  const [buttonClicked, setButtonClicked] = useState(false)
 
   const handlePostRequest = async () => {
+    setButtonClicked(true)
+    setYoutubeContent("")
+    setYoutubeSummary("")
     try {
       const response = await axios.post('http://127.0.0.1:8000/', {
         // Your POST data goes here
@@ -23,16 +30,38 @@ function App() {
     }
   };
 
+  const handleSummarizeRequest = async () => {
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/summarize', {
+        // Your POST data goes here
+        youtubeContent
+      });
+
+      setYoutubeSummary(response.data);
+
+    } catch (error) {
+      console.error('Error making POST request:', error);
+    }
+  };
+
   return (
     <>
       <h2>Enter YouTube URL</h2>
       <TextField id="outlined-basic" label="Youtube URL" variant="outlined" value={youtubeLink} onChange={(e)=>setYoutubeLink(e.target.value)}/>
       <Button variant="outlined" style={{margin: "12px"}} onClick={handlePostRequest}>Send</Button>
-      {youtubeContent && (
+      {youtubeContent ? (
         <div>
-          <h2>Response Data:</h2>
-          <b>{youtubeContent}</b>
+          <p>/ - COMPLETE - /</p>
+          <Button variant="outlined" style={{margin: "12px"}} onClick={handlePostRequest}>Ask Questions</Button>
+          <Button variant="outlined" style={{margin: "12px"}} onClick={handleSummarizeRequest}>Summarize</Button>
+          <Button variant="outlined" style={{margin: "12px"}} onClick={handlePostRequest}>Take Exam</Button>
+          <div>{youtubeSummary && youtubeSummary}</div>
         </div>
+        
+      ) : (
+        <div>
+          {buttonClicked && <CircularProgress />}
+        </div>  
       )}
     </>
   )
